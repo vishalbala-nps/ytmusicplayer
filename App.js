78b4770c2d037d8d13e64cc108a982da73c2027c
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2023-04-03 19:28:30 +0530

    Implemented List from youtube

--- App.js
+++ App.js
@@ -11,56 +11,88 @@ import {YT_SEARCH_API_KEY} from "@env";
 /*import {Button,TextInput} from 'react-native';
 import ytdl from "react-native-ytdl"
 import Player from './components/player.js';*/
-import Spinner from 'react-native-loading-spinner-overlay';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { TextInput,Button,FlatList } from 'react-native-paper';
+import { Provider as PaperProvider,TextInput,Button,List,ActivityIndicator } from 'react-native-paper';
+import { FlatList,Text } from 'react-native';
 
 function App(props) {
   const search = React.useRef("")
+  const scrollbegin = React.useRef(false)
   const [text, onChangeText] = React.useState();
   const [loading,setloading] = React.useReducer(function(state,val) {
     if (val.status === "loading") {
-      return {loading:true,error:false,data:[],nextpage:""}
+      return {loading:true,error:false,loading:false,data:[],nextpage:""}
+    } else if (val.status === "loading_np") {
+      return {error:false,loading:true,data:state.data,nextpage:state.nextpage}
     } else if (val.status === "error") {
       alert("An Error Occured. Please Try again later")
-      return {loading:false,error:true,data:[],nextpage:""}
+      return {error:true,loading:false,data:[],nextpage:""}
+    } else if (val.status === "data_np") {
+      return {error:false,loading:false,data:state.data.concat(val.data),nextpage:val.nextpage}
     } else {
-      return {loading:false,error:false,data:val.data.items,nextpage:""}
+      return {error:false,loading:false,data:val.data,nextpage:val.nextpage}
     }
-  },{loading:false,error:false,data:[],nextpage:""})
-  /*const [yturl,setyturl] = React.useReducer(function(state,val) {
-    if (val === "loading") {
-      return {loading:true,error:false,url:null}
-    } else if (val === "error") {
-      return {loading:false,error:true,url:null}
-    } else {
-      return {loading:false,error:false,url:val}
-    }
-  },{loading:false,error:false,url:null})*/
+  },{error:false,loading:false,data:[],nextpage:""})
+  function SongListItem(props) {
+    const litem = React.useMemo(function() {
+      return <List.Item title={props.song} description={props.artist}/>
+    },[props.song,props.artist])
+    return litem
+  }
   return (
     <PaperProvider>
-      <Spinner
-        visible={loading.loading}
-        textContent={'Please Wait'}
-      />
       <TextInput label="Search Youtube" onChangeText={function(t) {
         search.current = t;
       }}/>
       <Button onPress={function(){
-        console.log(process.env)
-        setloading({status:"loading"})
-        axios.get("https://www.googleapis.com/youtube/v3/search",{params:{
-          maxResults:20,
-          q:search.current,
-          type:"video,song",
-          key: process.env.YT_SEARCH_API_KEY
-        }}).then(function(r) {
-          setloading({status:"data",data:r.data.items,nextpage:r.data.nextPageToken})
-          setloading(r.data.items)
-        }).catch(function() {
-          setloading({status:"error"})
-        })
+        if (search.current !== "") {
+          setloading({status:"loading"})
+          axios.get("https://www.googleapis.com/youtube/v3/search",{params:{
+            maxResults:70,
+            q:search.current,
+            type:"video,song",
+            part:"snippet",
+            key: process.env.YT_SEARCH_API_KEY
+          }}).then(function(r) {
+            setloading({status:"data",data:r.data.items,nextpage:r.data.nextPageToken})
+          }).catch(function(e) {
+            console.log(e)
+            setloading({status:"error"})
+          })
+        }
       }}>Search</Button>
+      <FlatList data={loading.data} keyExtractor={function(item,index) {
+        return index
+      }} renderItem={function(item) {
+        return <SongListItem song={item.item.snippet.title} artist={item.item.snippet.channelTitle}/>
+      }} onEndReached={function() {
+        if (search.current !== "" && scrollbegin.current === true) {
+          console.log("End reached Loading")
+          setloading({status:"loading_np"})
+          if (loading.nextpage !== undefined) {
+            axios.get("https://www.googleapis.com/youtube/v3/search",{params:{
+              maxResults:70,
+              q:search.current,
+              type:"video,song",
+              part:"snippet",
+              pageToken:loading.nextpage,
+              key: process.env.YT_SEARCH_API_KEY
+            }}).then(function(r) {
+              setloading({status:"data_np",data:r.data.items,nextpage:r.data.nextPageToken})
+            }).catch(function(e) {
+              console.log(e)
+              setloading({status:"error"})
+            })
+          }
+        }
+      }} ListFooterComponent={function() {
+        if (loading.nextpage === undefined) {
+          return <Text style={{textAlign:"center"}}>You have reached the End</Text>
+        } else if (loading.loading) {
+          return <ActivityIndicator animating={true}/>
+        }
+      }} onMomentumScrollBegin={function() {
+        scrollbegin.current = true
+      }} onEndReachedThreshold={0.8}/>
       {/*<TextInput
         onChangeText={onChangeText}
         value={text}
