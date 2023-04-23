3abda1e3604d5497cfe3304ff7a021b5897a7911
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2023-04-23 16:35:07 +0530

    Implemented adding playlist

--- components/playlist.js
+++ components/playlist.js
@@ -1,50 +1,52 @@
-import { Button } from "react-native-paper"
+import { List,Dialog,Button,Text,TextInput } from "react-native-paper"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import React from "react"
-import TrackPlayer,{Event,useTrackPlayerEvents,State} from "react-native-track-player"
-import ytdl from 'react-native-ytdl'
+import { FlatList } from "react-native"
 export default function() {
-    const songlist = React.useRef([])
-    const stopped = React.useRef(false)
+    const [visible, setVisible] = React.useState(false);
+    const [plist,setplist] = React.useState([]) 
+    const ptex = React.useRef()
     React.useEffect(function() {
-        AsyncStorage.getItem("@pl_songs_test").then(function(r) {
-            songlist.current = JSON.parse(r)
-        }) 
-    },[])
-    useTrackPlayerEvents([Event.PlaybackTrackChanged,Event.PlaybackQueueEnded,Event.PlaybackError,Event.PlaybackState],async function(e) {
-        if (e.type === Event.PlaybackQueueEnded) {
-            stopped.current = true
-            console.log("Resetting")
-            TrackPlayer.reset()
-        } else if (e.type === Event.PlaybackState) {
-            console.log(e)
-        } else if (e.type === Event.PlaybackError) {
-            let queue = await TrackPlayer.getQueue()
-            alert("An error occured. Message "+e.message+"\nMusic JSON: "+JSON.stringify(queue))
-            console.log(e)
-        } else {
-            let nt = e.nextTrack+1
-            let queue = await TrackPlayer.getQueue()
-            if (nt < songlist.current.length && queue[nt] === undefined && e.nextTrack !== undefined && stopped.current === false) {
-                ytdl(songlist.current[nt].description, { quality: 'highestaudio' }).then(function(res) {
-                    console.log("got url adding to queue")
-                    let tobj = songlist.current[nt]
-                    tobj.url = res[0].url
-                    TrackPlayer.add(tobj)
-                    TrackPlayer.play()
-                })
-            }
-        } 
-    })
-    return <Button onPress={function() {
-        console.log("getting url for first song")
-        stopped.current = false
-        ytdl(songlist.current[0].description, { quality: 'highestaudio' }).then(function(res) {
-            console.log("got url adding to queue")
-            let tobj = songlist.current[0]
-            tobj.url = res[0].url
-            TrackPlayer.add(tobj)
-            TrackPlayer.play()
+        AsyncStorage.getItem("@playlists").then(function(d) {
+            console.log(d)
+            setplist(JSON.parse(d))
         })
-    }}>Play All</Button>
+    },[])
+    return (
+        <>
+            <Dialog visible={visible} onDismiss={function() {
+                setVisible(false)
+            }}>
+                <Dialog.Title>New Playlist</Dialog.Title>
+                <Dialog.Content>
+                    <TextInput label="Playlist Name" style={{backgroundColor:"#322b38"}} mode="outlined" onChangeText={function(t) {
+                        ptex.current = t
+                    }}/>
+                </Dialog.Content>
+                <Dialog.Actions>
+                    <Button onPress={async function() {
+                        const ps = await AsyncStorage.getItem("@playlists")
+                        let plist = JSON.parse(ps)
+                        if (plist === null) {
+                            plist = []
+                        } else {
+                            plist.push(ptex.current)
+                        }
+                        AsyncStorage.setItem("@playlists",JSON.stringify(plist))
+                        setVisible(false)
+                    }}>Done</Button>
+                </Dialog.Actions>
+            </Dialog>
+            <List.Item title="Create new Playlist" left={function() {
+                return <List.Icon icon="plus" />
+            }} onPress={function() {
+                setVisible(true)
+            }} />
+            <FlatList data={plist} keyExtractor={function(item,index) {
+                return index
+            }} renderItem={function(item) {
+                return <List.Item title={item.item} />
+            }}/>
+        </>
+    )
 }
