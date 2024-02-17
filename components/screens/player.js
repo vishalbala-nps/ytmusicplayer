6d79e055d49c8d8f939fd3110bbf94b1ee072d8d
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2024-02-17 21:30:15 +0530

    Fix crash

--- components/screens/player.js
+++ components/screens/player.js
@@ -21,44 +21,49 @@ export default function({navigation}) {
           navigation.navigate("Search")
         }
       })
-    return (
-        <>
-            <View style={{alignItems: 'center',width:"100%"}}>
-                <Image style={{width: 200, height: 200}} source={{uri:song.artwork}}/>
-                <Text />
-                <Text style={{fontSize:35,fontWeight:"bold",textAlign:"center"}} numberOfLines={2}>{song.title}</Text>
-                <Text style={{fontSize:20}}>{song.artist}</Text>
-                <View style={{flexDirection:"row",alignItems:"center"}}>
-                    <IconButton
-                        icon="skip-backward"
-                        size={45}
-                        onPress={function() {
-                            TrackPlayer.getCurrentTrack().then(function(ind) {
-                              const nind = ind-1
-                              if (nind >= 0) {
-                                TrackPlayer.skip(nind)
-                              }  
-                            })
-                        }}
-                        mode="contained"
-                    />
-                    <PlayPauseBtn/>
-                    <IconButton
-                        icon="skip-forward"
-                        size={45}
-                        onPress={async function() {
-                            const ind = await TrackPlayer.getCurrentTrack()
-                            const queue = await TrackPlayer.getQueue()
-                            const nind = ind+1
-                            if (nind < queue.length) {
-                                TrackPlayer.skip(nind)
-                            }
-                        }}
-                        mode="contained"
-                    />
+    console.log(song)
+    if (song === null) {
+        return null
+    } else {
+        return (
+            <>
+                <View style={{alignItems: 'center',width:"100%"}}>
+                    <Image style={{width: 200, height: 200}} source={{uri:song.artwork}}/>
+                    <Text />
+                    <Text style={{fontSize:35,fontWeight:"bold",textAlign:"center"}} numberOfLines={2}>{song.title}</Text>
+                    <Text style={{fontSize:20}}>{song.artist}</Text>
+                    <View style={{flexDirection:"row",alignItems:"center"}}>
+                        <IconButton
+                            icon="skip-backward"
+                            size={45}
+                            onPress={function() {
+                                TrackPlayer.getCurrentTrack().then(function(ind) {
+                                const nind = ind-1
+                                if (nind >= 0) {
+                                    TrackPlayer.skip(nind)
+                                }  
+                                })
+                            }}
+                            mode="contained"
+                        />
+                        <PlayPauseBtn/>
+                        <IconButton
+                            icon="skip-forward"
+                            size={45}
+                            onPress={async function() {
+                                const ind = await TrackPlayer.getCurrentTrack()
+                                const queue = await TrackPlayer.getQueue()
+                                const nind = ind+1
+                                if (nind < queue.length) {
+                                    TrackPlayer.skip(nind)
+                                }
+                            }}
+                            mode="contained"
+                        />
+                    </View>
                 </View>
-            </View>
-            <SeekBar />
-        </>
-    )
+                <SeekBar />
+            </>
+        )
+    }
 }
